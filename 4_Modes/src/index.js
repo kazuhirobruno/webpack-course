2b0aadc500527a3c72a6a8accf6a9160f7ce906d
Bruno <brunokazuhiro@gmail.com>
2026-01-03 16:59:06 -0300

    finish module 4: Webpack Modes

---
+++ 4_Modes/src/index.js
@@ -0,0 +1,9 @@
+import sum from "./calc";
+
+const x = sum(1, 2);
+const y = sum(10, 50);
+const z = sum(45, 84);
+
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ 4_Modes/webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    devMiddleware: {
+      index: false, // specify to enable root proxying
+    },
+    port: 8000,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new TerserPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      title: "Modes",
+    }),
+  ],
+};
